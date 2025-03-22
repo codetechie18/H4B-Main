c8100d3538a800a9c7336edadc53136d26522736
Harsh2227kumar <harsh2227kumar@gmail.com>
2025-03-22 23:06:11 +0530

    About Us Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import About from './Pages/About'
 const App = () => {
   return (
     <div>
-      
+      <About />
     </div>
   )
 }
---
+++ src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='bg-amber-700'>Hi</h1>
+    </div>
+  )
+}
+
+export default About
