c6f5d80b3faf0bc8d156a3d29c015b7f785c9db5
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2025-03-23 00:23:08 +0530

    feat:add the new page

---
+++ src/Pages/Text.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-center text-white">
+        Hack4Brahma
+      </h1>
+      
+    </div>
+  )
+}
+
+export default Text
