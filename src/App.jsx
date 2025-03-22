6214f711691818dedeca3dde0d306c7303f58b6d
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2025-03-23 01:29:04 +0530

    feat:add the new page (Prize)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home"; // Ensure you have this file
+import Prize from "./Pages/Prize"; // Ensure you have this file
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <div className="min-h-screen flex flex-col items-center justify-center">
                 <Routes>
                     <Route path="/home" element={<Home />} />
+                    <Route path="/prize" element={<Prize />} />
                    
                 </Routes>
             </div>
