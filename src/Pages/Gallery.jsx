107a753eeb8df7a312ca1a11b3814afb01f12cda
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-09 23:42:56 +0530

    gallery update

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,333 +1,121 @@
 import React from 'react';
-import { Users, Clock, Calendar, Handshake, CheckCircle } from 'lucide-react';
-import AnimatedTitle from "../Components/AnimatedTitle";
-
-const CornerBorderWrapper = ({ children }) => (
-  <div className="relative">
-  <div className="relative p-1 rounded-lg overflow-hidden border border-white">
-    {children}
-     
-    {/* Green border */}
-    <div className="absolute top-0 left-0 w-4 h-4 border-t-2 border-l-2 border-green-500 z-20" />
-    <div className="absolute top-0 right-0 w-4 h-4 border-t-2 border-r-2 border-green-500 z-20" />
-    <div className="absolute bottom-0 left-0 w-4 h-4 border-b-2 border-l-2 border-green-500 z-20" />
-    <div className="absolute bottom-0 right-0 w-4 h-4 border-b-2 border-r-2 border-green-500 z-20" />
-
-    {/* White border behind green */}
-    <div className="absolute top-0 left-0 w-4 h-4 border-t-2 border-l-2 border-white z-10" />
-    <div className="absolute top-0 right-0 w-4 h-4 border-t-2 border-r-2 border-white z-10" />
-    <div className="absolute bottom-0 left-0 w-4 h-4 border-b-2 border-l-2 border-white z-10" />
-    <div className="absolute bottom-0 right-0 w-4 h-4 border-b-2 border-r-2 border-white z-10" />
-  </div>
-  </div>
-);
 
 const Gallery = () => {
-  const stats = [
-    {
-      text: "500+ Hackers",
-      icon: <Users className="w-6 h-6" />
-    },
-    {
-      text: "24+ Hack Hours",
-      icon: <Clock className="w-6 h-6" />
-    },
-    {
-      text: "10+ Events",
-      icon: <Calendar className="w-6 h-6" />
-    },
-    {
-      text: "50+ Partners",
-      icon: <Handshake className="w-6 h-6" />
-    },
-    {
-      text: "10+ Successful Events",
-      icon: <CheckCircle className="w-6 h-6" />
-    }
-  ];
-
-  const images = [
-    { image: "img/img1.jpg" },
-    { image: "img/img2.jpg" },
-    { image: "img/img3.jpg" },
-    { image: "img/img4.jpg" },
-    { image: "img/img5.jpg" },
-    { image: "img/img6.jpg" },
-    { image: "img/game1.jpg" },
-    { image: "img/game2.jpg" },
-    { image: "img/workshop1.jpg" },
-    { image: "img/workshop2.jpg" },
-    { image: "img/talk.jpg" },
-    { image: "img/trophy.jpg" }
-  ];
-
-  // Mobile view component - shown only on small screens
-  const MobileGallery = () => (
-    <div className="block md:hidden">
-      <div className="grid grid-cols-1 gap-4">
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[0].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[0].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[0].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[2].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[1].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[1].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[3].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[1].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[2].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[2].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[4].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[3].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[3].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[5].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[6].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[4].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[4].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[7].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[8].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-      </div>
-    </div>
-  );
-
-  // Tablet view component
-  const TabletGallery = () => (
-    <div className="hidden sm:block md:hidden">
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[0].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[2].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[0].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[0].text}</p>
-          </div>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[1].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[1].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[3].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[1].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[5].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[4].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[2].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[2].text}</p>
-          </div>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[3].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[3].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[6].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[7].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-        <div>
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[4].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[4].text}</p>
-          </div>
-        </div>
-        <div>
-          <CornerBorderWrapper>
-            <img src={images[8].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-      </div>
-    </div>
-  );
-
   return (
-    <div className="container mx-auto px-2 py-10">
-      <div className="relative flex flex-col items-center text-[#198f51] mb-8">
-        <AnimatedTitle
-          title="<b>highlights</b>"
-          containerClass="mt-4 !text-black text-center reveal-element"
-        />
-      </div>
+    <div className="min-h-screen bg-[#0a0a0a] text-white py-16">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pl-6">
+          {/* First Row */}
+          <div className="rounded-xl overflow-hidden">
+  {/* Image */}
+  <div className="aspect-[4/3]">
+    <img
+      src="/img/img1.jpg"
+      alt="Hackers Coding"
+      className="w-full h-full object-cover"
+    />
+  </div>
 
-      {/* Mobile view */}
-      <MobileGallery />
-      
-      {/* Tablet view */}
-      <TabletGallery />
+  {/* Text below image */}
+  <div className="text-center mt-6">
+    <h2 className="text-[80px] font-bold text-green-500 leading-none">500+</h2>
+    <p className="text-3xl text-green-500 font-semibold">Hackers</p>
+  </div>
+</div>
 
-      {/* Desktop layout - EXACT SAME as original */}
-      <div className="hidden md:grid grid-cols-11 grid-rows-25 gap-4">
-        {/* Image Cards */}
-        <div className="col-span-5 row-span-2">
-          <CornerBorderWrapper>
-            <img src={images[0].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
 
-        <div className="col-span-4 row-span-2 col-start-5 row-start-3">
-          <CornerBorderWrapper>
-            <img src={images[1].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
+          <div className="rounded-xl row-span-4 h-auto mt- overflow-hidden">
+  {/* Top Image */}
+  <div className="h-[600px]">
+    <img
+      src="/img/game3.jpg"
+      alt="Team Work"
+      className="w-full h-full object-cover rounded-t-xl"
+    />
+  </div>
 
-        <div className="col-span-4 row-span-2 col-start-8 row-start-1">
-          <CornerBorderWrapper>
-            <img src={images[2].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
+  {/* Centered Text */}
+  <div className="p-6 text-center mt-6">
+    <h2 className="text-[80px] font-bold text-green-500 leading-none">24+</h2>
+    <p className="text-3xl text-green-500 font-semibold">Hack Hours</p>
+  </div>
 
-        {/* Stat Block */}
-        <div className="col-span-2 row-span-2 col-start-6 row-start-1">
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[0].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[0].text}</p>
+  {/* Bottom Image */}
+  <div className="mt-2">
+    <img
+      src="/img/workshop2.jpg"
+      alt="Bottom Image"
+      className="w-full h-90 object-cover rounded-b-xl"
+    />
+  </div>
+</div>
+
+
+
+          {/* Second Row */}
+          <div className="flex items-center justify-center bg-transparent text-center p-4 pl-6">
+            <div>
+              <h2 className="text-[80px] font-bold text-green-500 leading-none">50+</h2>
+              <p className="text-3xl text-green-500 font-semibold">Partners</p>
+            </div>
           </div>
-        </div>
 
-        <div className="col-span-4 row-span-2 col-start-1 row-start-3">
-          <CornerBorderWrapper>
-            <img src={images[3].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-
-        <div className="col-span-4 row-span-2 col-start-8 row-start-5">
-          <CornerBorderWrapper>
-            <img src={images[4].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-
-        {/* Stat Block */}
-        <div className="col-span-3 row-span-2 col-start-9 row-start-3">
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[1].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[1].text}</p>
+          <div className="relative overflow-hidden rounded-xl aspect-[4/3]">
+            <img
+              src="/img/img2.jpg"
+              alt="Speaker"
+              className="w-full h-full object-cover"
+            />
           </div>
-        </div>
 
-        <div className="col-span-4 row-span-3 col-start-1 row-start-5">
-          <CornerBorderWrapper>
-            <img src={images[5].image} className="w-full h-56 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
+          {/* Third Row */}
 
-        <div className="col-span-2 row-span-2 col-start-6 row-start-5">
-          <CornerBorderWrapper>
-            <img src={images[6].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-
-        <div className="col-span-3 row-span-2 col-start-9 row-start-7">
-          <CornerBorderWrapper>
-            <img src={images[7].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-
-        <div className="col-span-4 row-span-2 col-start-5 row-start-7">
-          <CornerBorderWrapper>
-            <img src={images[8].image} className="w-full h-48 object-cover rounded-lg" />
-          </CornerBorderWrapper>
-        </div>
-
-        {/* Stat Block */}
-        <div className="row-span-2 col-start-5 row-start-5">
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[3].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[3].text}</p>
+          <div>
+               <div className="relative overflow-hidden rounded-xl aspect-[4/3]">
+            <img
+              src="/img/img1.jpg"
+              alt="Hackers Coding"
+              className="w-full h-full object-cover"
+            />
           </div>
-        </div>
+            </div>
+            <div className="rounded-xl row-span-4 overflow-hidden">
+  {/* Image */}
+  <div className="h-[400px] mt-10">
+    <img
+      src="/img/game1.jpg"
+      alt="Team Work"
+      className="w-full h-full object-cover"
+    />
+  </div>
 
-        {/* Stat Block */}
-        <div className="row-span-3 col-start-2 row-start-7">
-          <div className="flex flex-col justify-center items-center h-full bg-transparent rounded-lg">
-            {stats[4].icon}
-            <p className="text-2xl font-semibold mt-2 text-green-800 text-center" style={{ fontFamily: 'Zentry-Regular' }}>{stats[4].text}</p>
+  {/* Text below image */}
+  <div className="text-center mt-40">
+    <h2 className="text-[80px] font-bold text-green-500 leading-none">10+</h2>
+    <p className="text-3xl text-green-500 font-semibold">Successful Event</p>
+  </div>
+</div>
+
+         
+
+          
+          
+         
+          
+ 
+   
+  
+ 
+
+
+          {/* Fourth Row */}
+          <div className="flex items-center justify-center bg-transparent gap-40 text-center p-4 pl-6">
+             <img
+              src="/img/img4.jpg"
+              alt="Group Photo"
+              className="w-full h-full object-cover"
+            /> 
+ 
           </div>
         </div>
       </div>
