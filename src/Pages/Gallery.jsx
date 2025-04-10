8ad846ca1eefd3a9e3a69619f74258efb034a90b
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-10 18:04:59 +0530

    Gallary Responsive

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -15,36 +15,41 @@ export default function Gallery() {
 
   const CornerBorderWrapper = ({ children }) => (
     <div className="relative">
-      <div className="relative p-1 rounded-lg overflow-hidden border border-white">
+      <div className="relative p-1 rounded-lg overflow-hidden border border-red">
         {children}
-        <div className="absolute top-0 left-0 w-4 h-4 border-t-2 border-l-2 border-green-500 z-20" />
-        <div className="absolute top-0 right-0 w-4 h-4 border-t-2 border-r-2 border-green-500 z-20" />
-        <div className="absolute bottom-0 left-0 w-4 h-4 border-b-2 border-l-2 border-green-500 z-20" />
-        <div className="absolute bottom-0 right-0 w-4 h-4 border-b-2 border-r-2 border-green-500 z-20" />
+        <div className="absolute top-0 left-0 w-3 h-3 sm:w-4 sm:h-4 border-t-2 border-l-2 border-green-500 z-20" />
+        <div className="absolute top-0 right-0 w-3 h-3 sm:w-4 sm:h-4 border-t-2 border-r-2 border-green-500 z-20" />
+        <div className="absolute bottom-0 left-0 w-3 h-3 sm:w-4 sm:h-4 border-b-2 border-l-2 border-green-500 z-20" />
+        <div className="absolute bottom-0 right-0 w-3 h-3 sm:w-4 sm:h-4 border-b-2 border-r-2 border-green-500 z-20" />
 
-        <div className="absolute top-0 left-0 w-4 h-4 border-t-2 border-l-2 border-white z-10" />
-        <div className="absolute top-0 right-0 w-4 h-4 border-t-2 border-r-2 border-white z-10" />
-        <div className="absolute bottom-0 left-0 w-4 h-4 border-b-2 border-l-2 border-white z-10" />
-        <div className="absolute bottom-0 right-0 w-4 h-4 border-b-2 border-r-2 border-white z-10" />
+        <div className="absolute top-0 left-0 w-3 h-3 sm:w-4 sm:h-4 border-t-2 border-l-2 border-white z-10" />
+        <div className="absolute top-0 right-0 w-3 h-3 sm:w-4 sm:h-4 border-t-2 border-r-2 border-white z-10" />
+        <div className="absolute bottom-0 left-0 w-3 h-3 sm:w-4 sm:h-4 border-b-2 border-l-2 border-white z-10" />
+        <div className="absolute bottom-0 right-0 w-3 h-3 sm:w-4 sm:h-4 border-b-2 border-r-2 border-white z-10" />
       </div>
     </div>
   );
 
   return (
-    <div className="relative min-h-screen bg-black text-white p-4 overflow-hidden">
-      <div className="text-center mb-8 mt-4">
+    <div className="relative min-h-screen bg-black text-white p-2 sm:p-4 overflow-hidden">
+      <div className="text-center mb-4 sm:mb-8 mt-2 sm:mt-4">
         <AnimatedTitle
           title="<b>Highlights</b>"
-          containerClass="mt-8 !text-black text-center reveal-element"
+          containerClass="mt-4 sm:mt-8 !text-black text-center reveal-element"
         />
       </div>
 
-      <div className="relative w-full mx-auto md:pl-24 px-2">
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-12 md:grid-rows-5 md:gap-4">
-          {/* Card 1 */}
-          <div className="md:col-span-5">
+      <div className="relative w-full mx-auto px-1 sm:px-2 md:pl-6 lg:pl-12 xl:pl-24">
+        {/* Use CSS Grid with responsive adjustments, but maintain original layout */}
+        <div className="grid grid-cols-1 gap-4 sm:gap-5 md:gap-6 
+                        sm:grid-cols-2 
+                        md:grid-cols-6 
+                        lg:grid-cols-12 lg:grid-rows-5">
+          
+          {/* Card 1 - Image */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-5">
             <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-60">
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-60">
                 <img
                   src="/img/img1.jpg"
                   alt="description"
@@ -54,36 +59,18 @@ export default function Gallery() {
             </CornerBorderWrapper>
           </div>
 
-          {/* Card 2 */}
-          <div className="md:col-span-5 md:col-start-4 md:row-start-2">
-            <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-60">
-                <img
-                  src="/img/img2.jpg"
-                  alt="description"
-                  className="w-full h-full object-cover rounded-md"
-                />
-              </div>
-            </CornerBorderWrapper>
+          {/* Card 9 - Stats Card */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-3 lg:col-start-6 lg:row-start-1">
+            <div className="bg-transperent text-green-500 font-bold flex flex-col justify-center items-center h-32 sm:h-40 md:h-full py-4 sm:py-6 md:py-0">
+              <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold">500+</span>
+              <span className="text-base sm:text-lg md:text-xl font-semibold mt-1">Hackers</span>
+            </div>
           </div>
 
-          {/* Card 3 */}
-          <div className="md:col-span-5 md:col-start-7 md:row-start-3">
+          {/* Card 4 - Tall Image */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-3 lg:row-span-3 lg:col-start-9 lg:row-start-1">
             <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-60">
-                <img
-                  src="/img/img3.jpg"
-                  alt="description"
-                  className="w-full h-full object-cover rounded-md"
-                />
-              </div>
-            </CornerBorderWrapper>
-          </div>
-
-          {/* Card 4 */}
-          <div className="md:col-span-3 md:row-span-3 md:col-start-9 md:row-start-1 h-full">
-            <CornerBorderWrapper>
-              <div className="h-full w-full">
+              <div className="h-40 sm:h-48 md:h-full w-full">
                 <img
                   src="/img/img4.jpg"
                   alt="description"
@@ -93,12 +80,20 @@ export default function Gallery() {
             </CornerBorderWrapper>
           </div>
 
-          {/* Card 5 */}
-          <div className="md:col-span-5 md:row-span-2 md:col-start-4 md:row-start-4">
+          {/* Card 10 - Stats Card */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-3 lg:col-start-1 lg:row-start-2">
+            <div className="bg-transperent text-green-500 font-bold flex flex-col justify-center items-center h-32 sm:h-40 md:h-full py-4 sm:py-6 md:py-0">
+              <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold">24+</span>
+              <span className="text-base sm:text-lg md:text-xl font-semibold mt-1">Hack Hours</span>
+            </div>
+          </div>
+
+          {/* Card 2 - Image */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-5 lg:col-start-4 lg:row-start-2">
             <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-60">
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-60">
                 <img
-                  src="/img/img5.jpg"
+                  src="/img/img2.jpg"
                   alt="description"
                   className="w-full h-full object-cover rounded-md"
                 />
@@ -106,10 +101,10 @@ export default function Gallery() {
             </CornerBorderWrapper>
           </div>
 
-          {/* Card 6 */}
-          <div className="md:col-span-3 md:row-span-3 md:col-start-1 md:row-start-3">
+          {/* Card 6 - Tall Image */}
+          <div className="sm:col-span-1 md:col-span-2 lg:col-span-3 lg:row-span-3 lg:col-start-1 lg:row-start-3">
             <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-full">
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-full">
                 <img
                   src="/img/img6.jpg"
                   alt="description"
@@ -119,10 +114,39 @@ export default function Gallery() {
             </CornerBorderWrapper>
           </div>
 
-          {/* Card 7 */}
-          <div className="md:col-span-4 md:row-span-4 md:col-start-1 md:row-start-5">
+          {/* Card 11 - Stats Card */}
+          <div className="sm:col-span-1 md:col-span-2 lg:col-span-3 lg:col-start-4 lg:row-start-3">
+            <div className="bg-transperent text-green-500 font-bold flex flex-col justify-center items-center h-32 sm:h-40 md:h-full py-4 sm:py-6 md:py-0">
+              <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold">24+</span>
+              <span className="text-base sm:text-lg md:text-xl font-semibold mt-1">Hack Hours</span>
+            </div>
+          </div>
+
+          {/* Card 3 - Image */}
+          <div className="sm:col-span-1 md:col-span-2 lg:col-span-5 lg:col-start-7 lg:row-start-3">
             <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-60">
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-60">
+                <img
+                  src="/img/img3.jpg"
+                  alt="description"
+                  className="w-full h-full object-cover rounded-md"
+                />
+              </div>
+            </CornerBorderWrapper>
+          </div>
+
+          {/* Card 12 - Stats Card */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-2 lg:row-span-3 lg:col-start-9 lg:row-start-3">
+            <div className="bg-transperent text-green-600 font-bold flex flex-col justify-center items-center h-32 sm:h-40 md:h-full py-4 sm:py-6 md:py-0">
+              <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold">50+</span>
+              <span className="text-base sm:text-lg md:text-xl font-semibold mt-1">Partners</span>
+            </div>
+          </div>
+
+          {/* Card 7 - Image */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-4 lg:row-span-2 lg:col-start-1 lg:row-start-5">
+            <CornerBorderWrapper>
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-60">
                 <img
                   src="/img/game1.jpg"
                   alt="description"
@@ -131,11 +155,34 @@ export default function Gallery() {
               </div>
             </CornerBorderWrapper>
           </div>
+          
+          {/* Card 13 - Stats Card */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-2 lg:row-span-2 lg:col-start-5 lg:row-start-4">
+            <div className="bg-transperent text-green-600 font-bold flex flex-col justify-center items-center h-32 sm:h-40 md:h-full py-4 sm:py-6 md:py-0">
+              <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold">10+</span>
+              <span className="text-base sm:text-lg md:text-xl font-semibold mt-1 text-center">
+                Successful Events
+              </span>
+            </div>
+          </div>
 
-          {/* Card 8 */}
-          <div className="md:col-span-5 md:row-span-2 md:col-start-7 md:row-start-5">
+          {/* Card 5 - Image */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-5 lg:row-span-2 lg:col-start-4 lg:row-start-4">
             <CornerBorderWrapper>
-              <div className="bg-white text-black font-bold text-xl flex justify-center items-center h-60">
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-60">
+                <img
+                  src="/img/img5.jpg"
+                  alt="description"
+                  className="w-full h-full object-cover rounded-md"
+                />
+              </div>
+            </CornerBorderWrapper>
+          </div>
+
+          {/* Card 8 - Image */}
+          <div className="sm:col-span-1 md:col-span-3 lg:col-span-5 lg:row-span-2 lg:col-start-7 lg:row-start-5">
+            <CornerBorderWrapper>
+              <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-40 sm:h-48 md:h-60">
                 <img
                   src="/img/workshop1.jpg"
                   alt="description"
@@ -144,59 +191,11 @@ export default function Gallery() {
               </div>
             </CornerBorderWrapper>
           </div>
-
-          {/* Card 9 */}
-          <div className="md:col-span-3 md:col-start-6 md:row-start-1">
-            <div className="bg-transperent text-green-500 font-bold flex flex-col justify-center items-center h-full py-8 md:py-0">
-              <span className="text-5xl md:text-7xl font-extrabold">500+</span>
-              <span className="text-xl font-semibold mt-1">Hackers</span>
-            </div>
-          </div>
-
-          {/* Card 10 */}
-          <div className="md:col-span-3 md:col-start-1 md:row-start-2">
-            <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-full">
-              <div className="bg-transperent text-green-500 font-bold flex flex-col justify-center items-center h-full py-8 md:py-0">
-                <span className="text-5xl md:text-7xl font-extrabold">24+</span>
-                <span className="text-xl font-semibold mt-1">Hack Hours</span>
-              </div>
-            </div>
-          </div>
-
-          {/* Card 11 */}
-          <div className="md:col-span-3 md:col-start-4 md:row-start-3">
-            <div className="bg-transperent text-black font-bold text-xl flex justify-center items-center h-full">
-              <div className="bg-transperent text-green-500 font-bold flex flex-col justify-center items-center h-full py-8 md:py-0">
-                <span className="text-5xl md:text-7xl font-extrabold">24+</span>
-                <span className="text-xl font-semibold mt-1">Hack Hours</span>
-              </div>
-            </div>
-          </div>
-
-          {/* Card 12 */}
-          <div className="md:col-span-2 md:row-span-3 md:col-start-9 md:row-start-3">
-            <div className="bg-transperent text-green-600 font-bold text-xl flex justify-center items-center h-full py-8 md:py-0">
-              <div className="flex flex-col justify-center items-center">
-                <span className="text-5xl md:text-7xl font-extrabold">50+</span>
-                <span className="text-xl font-semibold mt-1">Partners</span>
-              </div>
-            </div>
-          </div>
-
-          {/* Card 13 */}
-          <div className="md:col-span-2 md:row-span-5 md:col-start-5 md:row-start-5">
-            <div className="bg-transperent text-green-600 font-bold text-xl flex justify-center items-center h-full py-8 md:py-0">
-              <div className="flex flex-col justify-center items-center">
-                <span className="text-5xl md:text-7xl font-extrabold">10+</span>
-                <span className="text-xl font-semibold mt-1">
-                  Successful Events
-                </span>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-      <Footer />
+      <div className="mt-8 sm:mt-12">
+        <Footer />
+      </div>
     </div>
   );
 }
